579e2313b84ee8596ede57a4cb198d165dedccf8
Mario Juarez <mario@mjp.one>
2017-11-08 08:59:49 +0100

    v0.5.0

--- docs/src/app.js
+++ docs/src/app.js
@@ -1,5 +1,6 @@
 // Vue
 import Vue from 'vue'
+import moment from 'moment'
 import Datetime from '../../src'
 
 Vue.use(Datetime)
@@ -11,8 +12,14 @@ new Vue({
     return {
       date: '2017-09-25',
       time: '20:40',
+
       datetime: '2017-09-25T16:30:00Z',
+
       dateEmpty: '',
+      disabledDates: ['2017-09-07', ['2017-09-25', '2017-10-05']],
+      maxDate: moment().add(2, 'month').toISOString(),
+      minDate: moment().subtract(2, 'month').toISOString(),
+
       dateStyle: new Date().toISOString()
     }
   }
