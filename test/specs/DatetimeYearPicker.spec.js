84f2b041d578b3a212f8314898f625cfcdb8ffd4
Filippo Conti <filippo@codekraft.it>
2018-08-13 19:37:41 +0200

    Added tests

--- test/specs/DatetimeYearPicker.spec.js
+++ test/specs/DatetimeYearPicker.spec.js
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon'
 import { createVM } from '../helpers/utils.js'
 import DatetimeYearPicker from 'src/DatetimeYearPicker.vue'
 
@@ -42,6 +43,50 @@ describe('DatetimeYearPicker.vue', function () {
 
       vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item')[110].click()
       expect(vm.year).to.be.equal(2030)
+    })
+
+    it('should disable change event when year is disabled', function () {
+      const vm = createVM(this,
+        `<DatetimeYearPicker :min-date="minDate" :max-date="maxDate" :year="2018"></DatetimeYearPicker>`,
+        {
+          components: { DatetimeYearPicker },
+          data () {
+            return {
+              minDate: DateTime.fromISO('1920-01-01'),
+              maxDate: DateTime.fromISO('2000-12-01')
+            }
+          }
+        })
+      vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item')[0].click()
+      expect(vm.year).not.to.be.equal(1918)
+    })
+  })
+
+  describe('min/max validation', function () {
+    it('should add disabled class', function () {
+      const vm = createVM(this,
+        `<DatetimeYearPicker :min-date="minDate" :max-date="maxDate" :year="2018"></DatetimeYearPicker>`,
+        {
+          components: { DatetimeYearPicker },
+          data () {
+            return {
+              minDate: DateTime.fromISO('2018-01-01'),
+              maxDate: DateTime.fromISO('2018-12-01')
+            }
+          }
+        })
+
+      const years = vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item')
+      for (var i = 0; i < years.length; i++) {
+        const yearEl = years[i]
+        if (yearEl.textContent === '2018') {
+          expect(yearEl.className).to.have.string('vdatetime-year-picker__item--selected')
+          console.log(yearEl.textContent)
+          console.log(yearEl.className)
+          continue
+        }
+        expect(yearEl.className).to.have.string('vdatetime-year-picker__item--disabled')
+      }
     })
   })
 })
