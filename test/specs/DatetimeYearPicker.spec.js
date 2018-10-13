ae5ef118cc408c9788a6d9d3ddef35f51a7c5776
Mario Juarez <mario@mjp.one>
2018-10-14 00:39:59 +0200

    Update tests

--- test/specs/DatetimeYearPicker.spec.js
+++ test/specs/DatetimeYearPicker.spec.js
@@ -13,13 +13,40 @@ describe('DatetimeYearPicker.vue', function () {
 
       expect(vm.$('.vdatetime-year-picker')).to.exist
 
-      const years = vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item').map(el => parseInt(el.textContent))
+      const years = vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item')
+        .map(el => parseInt(el.textContent))
       const yearList = new Array(201).fill(null).map((item, index) => 1920 + index)
 
       expect(years).eql(yearList)
 
       const selected = parseInt(vm.$('.vdatetime-year-picker__item--selected').textContent)
       expect(selected).eql(2020)
+    })
+
+    it('should disable years', function () {
+      const vm = createVM(this,
+        `<DatetimeYearPicker :min-date="minDate" :max-date="maxDate" :year="2018"></DatetimeYearPicker>`,
+        {
+          components: { DatetimeYearPicker },
+          data () {
+            return {
+              minDate: DateTime.fromISO('2018-01-01T00:00:00.000Z').toUTC(),
+              maxDate: DateTime.fromISO('2018-12-01T00:00:00.000Z').toUTC()
+            }
+          }
+        })
+
+      const years = vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item')
+
+      years.forEach(year => {
+        const yearNumber = parseInt(year.textContent)
+
+        if (yearNumber === 2018) {
+          expect(year).to.have.not.class('vdatetime-year-picker__item--disabled')
+        } else {
+          expect(year).to.have.class('vdatetime-year-picker__item--disabled')
+        }
+      })
     })
   })
 
@@ -59,32 +86,6 @@ describe('DatetimeYearPicker.vue', function () {
         })
       vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item')[0].click()
       expect(vm.year).not.to.be.equal(1918)
-    })
-  })
-
-  describe('min/max validation', function () {
-    it('should add disabled class', function () {
-      const vm = createVM(this,
-        `<DatetimeYearPicker :min-date="minDate" :max-date="maxDate" :year="2018"></DatetimeYearPicker>`,
-        {
-          components: { DatetimeYearPicker },
-          data () {
-            return {
-              minDate: DateTime.fromISO('2018-01-01'),
-              maxDate: DateTime.fromISO('2018-12-01')
-            }
-          }
-        })
-
-      const years = vm.$$('.vdatetime-year-picker__list .vdatetime-year-picker__item')
-      for (var i = 0; i < years.length; i++) {
-        const yearEl = years[i]
-        if (yearEl.textContent === '2018') {
-          expect(yearEl.className).to.have.string('vdatetime-year-picker__item--selected')
-          continue
-        }
-        expect(yearEl.className).to.have.string('vdatetime-year-picker__item--disabled')
-      }
     })
   })
 })
